55b474f6bda6a6b12e29f900611f0c123c9b91c4
MarvelousMarcin <01159356@pw.edu.pl>
2021-01-21 05:15:05 +0100

    Aproksymator v.1

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 aprox: main.o splines.o points.o aproksymator_na_bazie.o gaus/libge.a
 	$(CC) -o aprox  main.o splines.o points.o aproksymator_na_bazie.o -L gaus -l ge
 
+aproxl: main.o splines.o points.o aproksymator_na_bazie_Laguerre.o gaus/libge.a
+	$(CC) -o aproxl main.o splines.o points.o aproksymator_na_bazie_Laguerre.o -L gaus -l ge
+
 intrp: main.o splines.o points.o interpolator.o gaus/libge.a
 	$(CC) -o intrp  main.o splines.o points.o interpolator.o -L gaus -l ge
 
@@ -10,10 +13,13 @@ prosta: main.o splines.o points.o prosta.o
 aproksymator_na_bazie.o: makespl.h points.h gaus/piv_ge_solver.h
 	$(CC) -I gaus -c aproksymator_na_bazie.c
 
+aproksymator_na_bazie_Laguerre.o: makespl.h points.h gaus/piv_ge_solver.h
+	$(CC) -I gaus -c aproksymator_na_bazie_Laguerre.c
+
 interpolator.o: makespl.h points.h gaus/piv_ge_solver.h
 	$(CC) -I gaus -c interpolator.c
 
 .PHONY: clean
 
 clean:
-	-rm *.o aprox intrp prosta
+	-rm *.o aprox intrp prosta aproxl
---
+++ aproksymator_na_bazie_Laguerre.c
@@ -0,0 +1,100 @@
+#include "makespl.h"
+#include "piv_ge_solver.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <float.h>
+
+/*Oblicza wartosc odpowieniej funkcji Laguerra dla x*/
+double fi(int n, double x)
+{
+    if(n == 0) return 1;
+    else if(n == 1) return 1-x;
+    else
+    {
+        int k = n - 1;
+        return (((2*k+1-x)*fi(k,x))-k*fi(k-1,x))/(k+1);
+    }
+}
+
+/*Pierwsza Pochodna*/
+double dfi(int n, double x)
+{
+    double delta = 1.0e-6;
+    double x1 = x - delta;
+    double x2 = x + delta;
+    double y1 = fi(n,x1);
+    double y2 = fi(n,x2);
+    return (y2 - y1) / (x2 - x1);
+}
+/*Druga Pochodna*/
+double d2fi(int n, double x)
+{
+    double delta = 1.0e-6;
+    double x1 = x - delta;
+    double x2 = x + delta;
+    double y1 = dfi(n,x1);
+    double y2 = dfi(n,x2);
+    return (y2 - y1) / (x2 - x1);
+}
+
+/*Trzecia Pochodna - Brakuje*/
+double d3fi(int n, double x)
+{
+
+
+}
+
+
+void make_spl(points_t * pts, spline_t * spl)
+{
+	matrix_t       *eqs= NULL;
+        double         *x = pts->x;
+        double         *y = pts->y;
+        double          a = x[0];
+        double          b = x[pts->n - 1];
+        int             i, j, k;
+        int             nb = pts->n - 3 > 10 ? 10 : pts->n - 3;
+  	char *nbEnv= getenv( "APPROX_BASE_SIZE" );
+
+        if( nbEnv != NULL && atoi( nbEnv ) > 0 )
+                nb = atoi( nbEnv );
+
+        eqs = make_matrix(nb, nb + 1);
+
+
+	for (j = 0; j < nb; j++)
+       	{
+		for (i = 0; i < nb; i++)
+			for (k = 0; k < pts->n; k++)
+				add_to_entry_matrix(eqs, j, i, fi(i, x[k]) * fi(j, x[k]));
+
+		for (k = 0; k < pts->n; k++)
+			add_to_entry_matrix(eqs, j, nb, y[k] * fi(j, x[k]));
+	}
+	
+	if (piv_ge_solver(eqs)) {
+		spl->n = 0;
+		return;
+	}
+
+	if (alloc_spl(spl, nb) == 0) {
+		for (i = 0; i < spl->n; i++) {
+			double xx = spl->x[i] = a + i*(b-a)/(spl->n-1);
+			xx+= 10.0*DBL_EPSILON;  // zabezpieczenie przed ulokowaniem punktu w poprzednim przedziale
+			spl->f[i] = 0;
+			spl->f1[i] = 0;
+			spl->f2[i] = 0;
+			spl->f3[i] = 0;
+			for (k = 0; k < nb; k++) 
+			{
+				double	ck = get_entry_matrix(eqs, k, nb);
+				spl->f[i]  += ck * fi  (k, xx);
+				spl->f1[i] += ck * dfi (k, xx);
+				spl->f2[i] += ck * d2fi(k, xx);
+				spl->f3[i] += ck * d2fi(k, xx); /*Podmienic na 3 pochodną*/
+
+			}
+		}
+	}
+}
